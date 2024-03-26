e4aeec3100ce7073399c5370ad041380d1f85e32
roamloki <154821759+roamloki@users.noreply.github.com>
2024-03-26 20:22:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION="3.9.8"
-DOWNLOAD_HOST="https://github.com/roamloki/minerproxysystem/linux"
+VERSION="3.9.9"
+DOWNLOAD_HOST="https://github.com/EvilGenius-dot/shortcut/raw/main/Readme/47/linux"
 ORIGIN_EXEC="minersys-${VERSION}"
 
 SERVICE_NAME="minersysservice"
